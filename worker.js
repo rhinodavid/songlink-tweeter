a3c5920e662cb3c87a18fb1630a413630da24468
David Walsh <dawalsh@gmail.com>
2016-10-17 16:04:26 -0700

    Refine stream setup

--- worker.js
+++ worker.js
@@ -8,14 +8,41 @@ const T = new Twit({
   access_token_secret: process.env.TWITTER_TOKEN_SECRET // eslint-disable-line
 });
 
-const stream = T.stream('statuses/filter', {track: ['open.spotify.com/track']});
+const startTime = Date.now();
+let tweetCount = 0;
+
+const stream = T.stream('statuses/filter', {
+  track: ['open spotify com track'],
+  // filter_level: 'low', //eslint-disable-line
+  language: 'en'
+});
 
 stream.on('connected', response => {
   console.log('Connected to statuses stream');
 });
 
 stream.on('tweet', tweet => {
+  if (tweet.retweeted_status === undefined) {
 
+    tweetCount++;
+
+    console.log('TWEET ID: ', tweet.id_str);
+    console.log(tweet.text);
+    console.log(tweet.entities.urls);
+    console.log('Tweet rate (T/M):', tweetCount / ((Date.now() - startTime) / 60000));
+    console.log('\n\n\n');
+  }
 });
 
-// stream.on('error')
+stream.on('error', error => {
+  console.log('ERROR:', error.message);
+  console.log('Status:', error.statusCode);
+});
+
+stream.on('disconnect', disconnectMessage => {
+  console.log('Disconnected with message:', disconnectMessage);
+});
+
+stream.on('limit', function (limitMessage) {
+  console.log('Limited with message:', limitMessage);
+});
