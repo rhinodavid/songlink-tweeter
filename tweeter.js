cbb40f6e3d70416c56b861f759533bf514df881f
David Walsh <dawalsh@gmail.com>
2016-10-18 13:33:04 -0700

    Promisify postResponse

--- tweeter.js
+++ tweeter.js
@@ -4,12 +4,31 @@ const getSonglinkForSpotify = require('./helpers/tweeterHelpers').getSonglinkFor
 const getSpotifyId = require('./helpers/tweeterHelpers').getSpotifyId;
 
 const postResponse = tweet => {
-  getSonglinkForSpotify(getSpotifyId(tweet.song_url))
-  .then(songlink => {
-    const songlinkUrl = songlink.share_link;
-    // take songlink Url and make a tweet out of it
-  })
-  .catch(err => {
-    winston.log('error', 'Error getting Songlink data\n', err);
+  return new Promise((resolve, reject) => {
+    getSonglinkForSpotify(getSpotifyId(tweet.song_url))
+      .then(songlink => {
+        const songlinkUrl = songlink.share_link;
+        const replyTweet = {
+          'in_reply_to_status_id': tweet.id_str,
+          'status': `@${ tweet.username } share this track with all your friends with Songlink ${ songlinkUrl }`
+        };
+        T.post('statuses/update', replyTweet, (error, data, response) => {
+          if (error) {
+            winston.log('error', 'Error posting reply tweet');
+            winston.log('error', 'Tweet:', tweet);
+            winston.log('error', 'Twitter error:', error);
+            reject(error);
+          }
+          resolve(data);
+        });
+      })
+      .catch(err => {
+        winston.log('error', 'Error getting Songlink data\n', err);
+        resolve(err);
+      });
   });
+};
+
+module.exports = {
+  postResponse
 };
