d8277a16011263c1949ff4204b17e9c274907fdc
David Walsh <dawalsh@gmail.com>
2016-10-18 13:32:46 -0700

    Make keys string literals

--- db/db.js
+++ db/db.js
@@ -8,12 +8,12 @@ const db = new Sequelize('sltweeter', 'user', null,
 
 
 const Tweet = db.define('Tweet', {
-  id_str: { // eslint-disable-line
+  'id_str': {
     type: Sequelize.STRING,
     primaryKey: true
   },
-  song_url: Sequelize.STRING, // eslint-disable-line
-  username: Sequelize.STRING
+  'song_url': Sequelize.STRING,
+  'username': Sequelize.STRING
 });
 
 Tweet.sync();
