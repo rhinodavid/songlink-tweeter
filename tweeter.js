6f85b7adc7ca1facc9d6085e3d9c70b8d613330e
David Walsh <dawalsh@gmail.com>
2016-10-18 16:03:05 -0700

    Move postResponse to helper and implement getQueuedTweetsAndRespond

--- tweeter.js
+++ tweeter.js
@@ -1,34 +1,15 @@
 const T = require('./twitter/twitterConnection');
 const winston = require('./util/logger.js');
-const getSonglinkForSpotify = require('./helpers/tweeterHelpers').getSonglinkForSpotify;
-const getSpotifyId = require('./helpers/tweeterHelpers').getSpotifyId;
+const getQueuedTweetsAndEmptyQueue = require('./helpers/dbHelpers').getQueuedTweetsAndEmptyQueue;
+const postResponseTweet = require('./helpers/apiHelpers').postResponseTweet;
 
-const postResponse = tweet => {
-  return new Promise((resolve, reject) => {
-    getSonglinkForSpotify(getSpotifyId(tweet.song_url))
-      .then(songlink => {
-        const songlinkUrl = songlink.share_link;
-        const replyTweet = {
-          'in_reply_to_status_id': tweet.id_str,
-          'status': `@${ tweet.username } share this track with all your friends with Songlink ${ songlinkUrl }`
-        };
-        T.post('statuses/update', replyTweet, (error, data, response) => {
-          if (error) {
-            winston.log('error', 'Error posting reply tweet');
-            winston.log('error', 'Tweet:', tweet);
-            winston.log('error', 'Twitter error:', error);
-            reject(error);
-          }
-          resolve(data);
-        });
-      })
-      .catch(err => {
-        winston.log('error', 'Error getting Songlink data\n', err);
-        resolve(err);
-      });
+const getQueuedTweetsAndRespond = function() {
+  getQueuedTweetsAndEmptyQueue()
+  .then(tweets => {
+    tweets.forEach(tweet => {
+      postResponseTweet(tweet);
+    });
   });
 };
 
-module.exports = {
-  postResponse
-};
+module.exports = getQueuedTweetsAndRespond;
