eb22866719ea066795f775606f6fe01e6cf340f4
David Walsh <dawalsh@gmail.com>
2016-10-17 22:03:42 -0700

    Begin implementing postResponse

--- tweeter.js
+++ tweeter.js
@@ -1,6 +1,15 @@
 const T = require('./twitter/twitterConnection');
 const winston = require('./util/logger.js');
+const getSonglinkForSpotify = require('./helpers/tweeterHelpers').getSonglinkForSpotify;
+const getSpotifyId = require('./helpers/tweeterHelpers').getSpotifyId;
 
 const postResponse = tweet => {
-
+  getSonglinkForSpotify(getSpotifyId(tweet.song_url))
+  .then(songlink => {
+    const songlinkUrl = songlink.share_link;
+    // take songlink Url and make a tweet out of it
+  })
+  .catch(err => {
+    winston.log('error', 'Error getting Songlink data\n', err);
+  });
 };
