ae39e8dd83cda76cc8659714262a04aadb25e92e
David Walsh <dawalsh@gmail.com>
2016-12-13 13:34:55 -0800

    Conform to airbnb style guide

--- stream.js
+++ stream.js
@@ -4,28 +4,28 @@ const winston = require('./util/logger.js');
 
 const stream = T.stream('statuses/filter', {
   track: ['open spotify com track'],
-  language: 'en'
+  language: 'en',
 });
 
-stream.on('connected', response => {
+stream.on('connected', () => {
   winston.log('info', 'Connected to statuses stream');
 });
 
-stream.on('tweet', tweet => {
+stream.on('tweet', (tweet) => {
   enqueueTweet(tweet);
 });
 
-stream.on('error', error => {
+stream.on('error', (error) => {
   winston.log('error', 'Stream error:');
   winston.log('error', 'Message:', error.message);
   winston.log('error', 'Status code:', error.statusCode);
 });
 
-stream.on('disconnect', disconnectMessage => {
+stream.on('disconnect', (disconnectMessage) => {
   winston.log('info', 'Disconnected with message:', disconnectMessage);
 });
 
-stream.on('limit', function (limitMessage) {
+stream.on('limit', (limitMessage) => {
   winston.log('error', 'Rate limited with message:', limitMessage);
 });
 
