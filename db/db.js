fcece2b4c3e88c4cd76157876713bda11bcd6a9f
David Walsh <dawalsh@gmail.com>
2016-12-13 13:48:21 -0800

    Conform to airbnb style guide

--- db/db.js
+++ db/db.js
@@ -1,23 +1,42 @@
 const Sequelize = require('sequelize');
+
 const db = new Sequelize('sltweeter', 'user', null,
   {
     dialect: 'sqlite',
     storage: './db/sltweeter.sqlite',
-    logging: false
+    logging: false,
   });
 
 
 const Tweet = db.define('Tweet', {
-  'id_str': {
+  id_str: {
     type: Sequelize.STRING,
-    primaryKey: true
+    primaryKey: true,
   },
-  'song_url': Sequelize.STRING,
-  'username': Sequelize.STRING
+  song_url: Sequelize.STRING,
+  username: Sequelize.STRING,
+});
+
+const ConfigItem = db.define('ConfigItem', {
+  key: Sequelize.STRING,
+  value: Sequelize.STRING,
+});
+
+const IgnoredUser = db.define('IgnoredUser', {
+  id_str: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  username: Sequelize.STRING,
+  tweet_id_str: Sequelize.STRING,
 });
 
 Tweet.sync();
+ConfigItem.sync();
+IgnoredUser.sync();
 
 module.exports = {
-  Tweet
+  Tweet,
+  ConfigItem,
+  IgnoredUser,
 };
