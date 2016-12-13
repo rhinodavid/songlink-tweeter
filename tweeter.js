ceb96e73ecbda31cc89034521a8c6e23ba01343e
David Walsh <dawalsh@gmail.com>
2016-12-13 13:38:25 -0800

    Conform to airbnb style guide

--- tweeter.js
+++ tweeter.js
@@ -1,12 +1,10 @@
-const T = require('./twitter/twitterConnection');
-const winston = require('./util/logger.js');
 const getQueuedTweetsAndEmptyQueue = require('./helpers/dbHelpers').getQueuedTweetsAndEmptyQueue;
 const postResponseTweet = require('./helpers/apiHelpers').postResponseTweet;
 
-const getQueuedTweetsAndRespond = function() {
+const getQueuedTweetsAndRespond = function getQueuedTweetsAndRespond() {
   getQueuedTweetsAndEmptyQueue()
-  .then(tweets => {
-    tweets.forEach(tweet => {
+  .then((tweets) => {
+    tweets.forEach((tweet) => {
       postResponseTweet(tweet);
     });
   });
