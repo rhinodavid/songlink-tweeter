4c329290bb53c638fda7bc06ecc148779328f56f
David Walsh <dawalsh@gmail.com>
2016-12-13 17:26:12 -0800

    Add helpers for top tweets

--- helpers/dbHelpers.js
+++ helpers/dbHelpers.js
@@ -1,13 +1,13 @@
 const Tweet = require('../db/db').Tweet;
 
-const validSpotifyUrl = function(url) {
+const validSpotifyUrl = function validSpotifyUrl(url) {
   return /^https:\/\/open\.spotify\.com\/track\/\w+$/.test(url);
 };
 
-const enqueueTweet = function(tweet) {
+const enqueueTweet = function enqueueTweet(tweet) {
   if (tweet.retweeted_status) {
     // only enqueue tweets that are not retweets
-    return;
+    return new Promise(resolve => resolve(null));
   }
   // check each URL to see if there is a valid spotify link
   const foundSpotifyUrlIndex = tweet.entities.urls.reduce((foundIndex, url, index) => {
@@ -18,35 +18,54 @@ const enqueueTweet = function(tweet) {
   }, -1);
 
   if (foundSpotifyUrlIndex > -1) {
+    // found a valid spotify URL in the tweet -- enqueue it
     return Tweet.upsert({
       id_str: tweet.id_str, // eslint-disable-line
       song_url: tweet.entities.urls[foundSpotifyUrlIndex].expanded_url, // eslint-disable-line
-      username: tweet.user.screen_name
-    });
-  } else {
-    return new Promise((resolve, reject) => {
-      resolve(null);
-      reject(null);
+      username: tweet.user.screen_name,
+      user_verified: tweet.user.verified,
+      user_followers_count: tweet.user.followers_count,
     });
   }
+
+  return new Promise(resolve => resolve(null));
 };
 
 const getQueuedTweetsAndEmptyQueue = () => {
   return new Promise((resolve, reject) => {
-    Tweet.findAll({raw: true})
-      .then(tweets => {
-        Tweet.destroy({where: {}}).
-        then(() => {
+    Tweet.findAll({})
+      .then((tweets) => {
+        Tweet.destroy({ where: {} })
+        .then(() => {
           resolve(tweets);
         });
       })
-      .catch(error => {
+      .catch((error) => {
         reject(error);
       });
   });
 };
 
+const getVerifiedTweets = () =>
+  Tweet.findAll({
+    where: {
+      user_verified: true,
+    },
+  });
+
+const getTweetsFromPopularUsers = () =>
+  Tweet.findAll({
+    limit: 5,
+    order: '`user_followers_count` DESC',
+  });
+
+const emptyTweetQueue = () =>
+  Tweet.destroy({ where: {} });
+
 module.exports = {
   enqueueTweet,
-  getQueuedTweetsAndEmptyQueue
+  getQueuedTweetsAndEmptyQueue,
+  emptyTweetQueue,
+  getVerifiedTweets,
+  getTweetsFromPopularUsers,
 };
