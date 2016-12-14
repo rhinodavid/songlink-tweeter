512c41c08b74f734a9c183fa38f96a14a69fd5ee
David Walsh <dawalsh@gmail.com>
2016-12-13 17:24:39 -0800

    Modify tweeter to only tweet top tweet

--- tweeter.js
+++ tweeter.js
@@ -1,12 +1,28 @@
-const getQueuedTweetsAndEmptyQueue = require('./helpers/dbHelpers').getQueuedTweetsAndEmptyQueue;
+const dbHelpers = require('./helpers/dbHelpers');
+const logger = require('./util/logger.js');
 const postResponseTweet = require('./helpers/apiHelpers').postResponseTweet;
 
 const getQueuedTweetsAndRespond = function getQueuedTweetsAndRespond() {
-  getQueuedTweetsAndEmptyQueue()
-  .then((tweets) => {
-    tweets.forEach((tweet) => {
-      postResponseTweet(tweet);
-    });
+  return Promise.all([
+    dbHelpers.getVerifiedTweets(),
+    dbHelpers.getTweetsFromPopularUsers(),
+  ])
+  .then(([verifiedTweets, popularTweets]) => {
+    if (verifiedTweets.length) {
+      // tweet back to a verified user
+      return postResponseTweet(verifiedTweets[Math.floor(Math.random() * verifiedTweets.length)]);
+    } else if (popularTweets.length) {
+      // tweet to the most followed user
+      return postResponseTweet(popularTweets[0]);
+    }
+    return null;
+  })
+  // eslint-disable-next-line no-confusing-arrow
+  .then(responseData => responseData ? logger.info('Successfully tweeted') : null)
+  .then(() => dbHelpers.emptyTweetQueue())
+  .catch((error) => {
+    dbHelpers.emptyTweetQueue();
+    logger.error('Error with tweeter worker', error);
   });
 };
 
